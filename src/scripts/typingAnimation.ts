47f8847e113d5751b8e3e9f7467bad709afea993
cairaaa <200700471+cairaaa@users.noreply.github.com>
2026-03-22 16:48:58 -0400

    add typing animation script

---
+++ src/scripts/typingAnimation.ts
@@ -0,0 +1,14 @@
+import Typed from "typed.js";
+
+new Typed("#typed-roles", {
+  strings: [
+    "full stack developer",
+    "app creator",
+    "TypeScript enthusiast",
+    "problem solver"
+  ],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 2000,
+  loop: true
+});
